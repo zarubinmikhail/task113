5fcf96eb15591b366efdd176a1c74fb196e9b6c6
fradkinshtein <78609486+fradkinshtein@users.noreply.github.com>
2021-05-25 20:40:01 +0300

    r1

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,13 +4,18 @@ import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    UserDao userDaoJDBC = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        userDaoJDBC.createUsersTable();
+        try {
+            userDaoJDBC.createUsersTable();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
--- target/classes/test.sql
+++ target/classes/test.sql
@@ -1,1 +1,1 @@
-DROP TABLE users
+
